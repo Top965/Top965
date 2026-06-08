6a9d6958cbf3c1ade9b709a2f21cf5dc63f2677d
Top965 <wael.lahlabat@gmail.com>
2026-06-08 21:04:02 +0300

    Fix redirect method in auth callback route

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -11,5 +11,5 @@ export async function GET(request: NextRequest) {
     const supabase = createRouteHandlerClient({ cookies })
     await supabase.auth.exchangeCodeForSession(code)
   }
-return nextResponse.redirect(requestUrl.origin)
+return NextResponse.redirect(requestUrl.origin)
   }
